100925482feeb6c68d623e4709bc3308f826b528
Hermosilla <hermosilladiaz@gmail.com>
2020-09-20 11:57:27 -0500

    Delete test1.py

--- test1.py
+++
@@ -1,6 +0,0 @@
-import bokeh.plotting as bpl
-b = bpl.figure()
-b.circle(x=[10,20,40],y=[800,400,650],radius=[10,20,10],color=["red","purple","orange"], line_color="black",line_width=5)
-
-output_file('circulo.html')
-bpl.show(b)
